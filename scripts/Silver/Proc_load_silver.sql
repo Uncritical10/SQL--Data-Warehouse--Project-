c4b469afe19d51da1228121a74c18d71d21b9296
Uncritical10 <syedamanali1910@gmail.com>
2026-02-26 00:25:24 +0530

    Create Proc_load_silver.sql

---
+++ scripts/Silver/Proc_load_silver.sql
@@ -0,0 +1,138 @@
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME = GETDATE();
+    
+    BEGIN TRY
+        -- Ensure all-or-nothing execution
+        BEGIN TRANSACTION;
+
+        PRINT '---------------------------------------------------';
+        PRINT 'Loading Silver Layer';
+        PRINT '---------------------------------------------------';
+
+        -- 1. Table: Silver.crm_cust_info
+        PRINT '>> Truncating and Inserting: Silver.crm_cust_info';
+        TRUNCATE TABLE Silver.crm_cust_info;
+        INSERT INTO Silver.crm_cust_info (
+            cst_id, cst_key, cst_firstname, cst_lastname, 
+            cst_marital_status, cst_gender, cst_create_date
+        )
+        SELECT 
+            cst_id,
+            cst_key,
+            TRIM(cst_firstname),
+            TRIM(cst_lastname),
+            CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+                 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married' 
+                 ELSE 'n/a'
+            END,
+            CASE WHEN UPPER(TRIM(cst_gender)) = 'F' THEN 'Female'
+                 WHEN UPPER(TRIM(cst_gender)) = 'M' THEN 'Male' 
+                 ELSE 'n/a'
+            END,
+            cst_create_date
+        FROM bronze.crm_cust_info;
+
+        -- 2. Table: silver.crm_prd_info
+        PRINT '>> Truncating and Inserting: silver.crm_prd_info';
+        TRUNCATE TABLE silver.crm_prd_info;
+        INSERT INTO silver.crm_prd_info (
+            prd_id, cat_id, prd_key, prd_nm, 
+            prd_cost, prd_line, prd_start_dt, prd_end_dt
+        )
+        SELECT
+            prd_id,
+            REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_'), 
+            SUBSTRING(prd_key, 7, LEN(prd_key)),       
+            prd_nm,
+            ISNULL(prd_cost, 0),
+            CASE 
+                WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+                WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+                WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+                WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+                ELSE 'n/a'
+            END, 
+            CAST(prd_start_dt AS DATE),
+            CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE)
+        FROM bronze.crm_prd_info;
+
+        -- 3. Table: Silver.crm_sales_details
+        PRINT '>> Truncating and Inserting: Silver.crm_sales_details';
+        TRUNCATE TABLE Silver.crm_sales_details;
+        INSERT INTO Silver.crm_sales_details (
+            sls_ord_num, sls_prd_key, sls_cust_id, 
+            sls_order_dt, sls_ship_dt, sls_due_dt, 
+            sls_sales, sls_quantity, sls_price
+        )
+        SELECT 
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id,
+            CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+                 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE) END,
+            CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+                 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE) END,
+            CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+                 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE) END, 
+            CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
+                 THEN sls_quantity * ABS(sls_price)
+                 ELSE sls_sales END,
+            sls_quantity,
+            CASE WHEN sls_price IS NULL OR sls_price <= 0 
+                 THEN (CASE WHEN sls_sales IS NULL OR sls_sales <= 0 THEN 0 ELSE sls_sales END) / NULLIF(sls_quantity, 0)
+                 ELSE sls_price END
+        FROM bronze.crm_sales_details;
+
+        -- 4. Table: silver.erp_cust_az12
+        PRINT '>> Truncating and Inserting: silver.erp_cust_az12';
+        TRUNCATE TABLE Silver.erp_cust_az12;
+        INSERT INTO Silver.erp_cust_az12(cid, bdate, gen)
+        SELECT 
+            CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid)) ELSE cid END,
+            CASE WHEN bdate > GETDATE() THEN NULL ELSE bdate END,
+            CASE 
+                WHEN UPPER(TRIM(REPLACE(REPLACE(gen, CHAR(13), ''), CHAR(10), ''))) IN ('F', 'FEMALE') THEN 'Female'
+                WHEN UPPER(TRIM(REPLACE(REPLACE(gen, CHAR(13), ''), CHAR(10), ''))) IN ('M', 'MALE')   THEN 'Male'
+                ELSE 'n/a'
+            END
+        FROM bronze.erp_cust_az12;
+
+        -- 5. Table: Silver.erp_loc_a101
+        PRINT '>> Truncating and Inserting: Silver.erp_loc_a101';
+        TRUNCATE TABLE Silver.erp_loc_a101;
+        INSERT INTO Silver.erp_loc_a101(cid, country)
+        SELECT 
+            REPLACE(cid, '-', ''),
+            CASE
+                WHEN UPPER(TRIM(REPLACE(REPLACE(country, CHAR(13), ''), CHAR(10), ''))) = 'DE' THEN 'Germany'
+                WHEN UPPER(TRIM(REPLACE(REPLACE(country, CHAR(13), ''), CHAR(10), ''))) IN ('US', 'USA') THEN 'United States'
+                WHEN country IS NULL OR TRIM(REPLACE(REPLACE(country, CHAR(13), ''), CHAR(10), '')) = '' THEN 'n/a'
+                ELSE TRIM(country)
+            END
+        FROM bronze.erp_loc_a101;
+
+        -- 6. Table: Silver.erp_px_cat_g1v2
+        PRINT '>> Truncating and Inserting: Silver.erp_px_cat_g1v2';
+        TRUNCATE TABLE Silver.erp_px_cat_g1v2;
+        INSERT INTO Silver.erp_px_cat_g1v2(id, cat, subcat, maintenance)
+        SELECT id, cat, subcat, maintenance FROM bronze.erp_px_cat_g1v2;
+
+        -- Commit changes if everything is successful
+        COMMIT TRANSACTION;
+
+        PRINT '---------------------------------------------------';
+        PRINT 'Silver Layer Loaded Successfully in ' + CAST(DATEDIFF(second, @start_time, GETDATE()) AS VARCHAR) + ' seconds';
+        PRINT '---------------------------------------------------';
+
+    END TRY
+    BEGIN CATCH
+        -- Rollback if any error occurs
+        IF @@TRANCOUNT > 0 ROLLBACK TRANSACTION;
+
+        PRINT '###################################################';
+        PRINT 'ERROR OCCURRED DURING LOADING';
+        PRINT 'Error Message: ' + ERROR_MESSAGE();
+        PRINT '###################################################';
+    END CATCH
+END;
